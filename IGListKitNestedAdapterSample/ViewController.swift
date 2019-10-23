7a639c5852c49913c279bdef70bf2cfdd40fcf34
Isao Kono (isaoeka) <isaoeka@gmail.com>
2019-10-23 21:20:07 +0900

    :recycle: Add IGListKit with carthage

--- IGListKitNestedAdapterSample/ViewController.swift
+++ IGListKitNestedAdapterSample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IGListKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,4 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
-
